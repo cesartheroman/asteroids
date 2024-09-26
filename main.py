b89f7735516ec1d3d00a8e37e73c6bea18006116
cesartheroman <cesardroman18@gmail.com>
2024-09-26 16:47:26 -0500

    feat: implements destruction of asteroid logic

--- main.py
+++ main.py
@@ -42,6 +42,11 @@ def main():
                 print('Game over!')
                 sys.exit()
 
+            for shot in shots:
+                if shot.did_collide(asteroid):
+                    shot.kill()
+                    asteroid.kill()
+
         screen.fill('black')
 
         for obj in drawable:
