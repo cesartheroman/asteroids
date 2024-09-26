e2f5437b676b27dd2ca30cc6f596d82bd4357a5f
cesartheroman <cesardroman18@gmail.com>
2024-09-26 16:22:26 -0500

    feat: implements shooting logic

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import *
 from player import Player
+from shot import Shot
 
 def main():
     #game init
@@ -15,12 +16,14 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
-    asteroid_field = AsteroidField()
-
     Player.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     dt = 0
