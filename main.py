3c9226963583bf90782cce69e20e2ed71402209c
cesartheroman <cesardroman18@gmail.com>
2024-09-26 12:16:53 -0500

    feat: implement delta time logic

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import pygame
 from constants import *
 
 def main():
+    #game init
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    #handling fps
+    clock = pygame.time.Clock()
+    dt = 0
+
+    #game loop start
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -12,6 +18,8 @@ def main():
 
         screen.fill('black')
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__== "__main__":
